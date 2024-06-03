00c0fc809a6bdebe14ae22e0bf9fa93410da744a
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-06-03 14:52:38 +0530

    Adding Vitest and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(`Hello via Bun! => ${Bun.env.SECRET}`);
+import { Hono } from 'hono'
+import { etag } from 'hono/etag'
+import { logger } from 'hono/logger'
+const app = new Hono
+app.use(etag(), logger())
+app.get('/', (c) => c.text('Hono!'))
+
+export default app
